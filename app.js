15d4b0e87b3c8539d508a4dac73206ab5e226885
CARLOS CASTILLO BARRADAS <zS23014088@estudiantes.uv.mx>
2025-10-07 22:26:33 -0600

    Actualizar: se acutualizaron los archivos para mitigar vulnerabilidades

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 require('dotenv').config();
 const path = require('path');
 const express = require('express');
+const helmet = require('helmet');
 const db = require('./db');
 
 const app = express();
+app.use(helmet());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Ruta vulnerable a inyección SQL (intencional)
-app.get('/users', (req, res) => {
+// Ruta segura con consultas parametrizadas
+app.get('/users-safe', (req, res) => {
   const name = req.query.name || '';
-  const sql = `SELECT id, name, email FROM users WHERE name LIKE '%${name}%'`; // VULNERABLE
-  db.all(sql, (err, rows) => {
+  const sql = "SELECT id, name, email FROM users WHERE name LIKE ?";
+  db.all(sql, [`%${name}%`], (err, rows) => {
     if (err) return res.status(500).send('Error DB');
     res.render('users', { rows, q: name });
   });
 });
 
-// XSS reflejado (eco sin escapar)
 app.get('/echo', (req, res) => {
   const msg = req.query.msg || '';
-  res.send(`<h1>Echo</h1><p>${msg}</p>`); // VULNERABLE
+  res.render('echo', { msg });
 });
 
 app.get('/', (req, res) => res.render('index'));
